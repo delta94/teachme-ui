87142654fa13270e9940f4a559350bb0384c2461
Nofar Baranes <nofar@reallygood.co.il>
2020-05-23 11:41:42 +0300

    rendering quiz iframe in quiz's screen.

--- src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
+++ src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { TeachMeContext } from "../../../../App";
 
 export default function QuizScreen() {
-  return <section className="screen quiz-screen">QuizScreen</section>;
+  const { sidebar } = useContext(TeachMeContext);
+  const { isOpen, setIsOpen } = sidebar;
+
+  useEffect(() => {
+    if (isOpen) {
+      setTimeout(() => {
+        setIsOpen(false);
+      }, 0);
+    }
+  }, []);
+
+  return (
+    <section className="screen quiz-screen">
+      <iframe
+        src="http://localhost:9001/?platform=mock&teachme=mock&courseId=1#"
+        frameBorder="0"
+      ></iframe>
+    </section>
+  );
 }
