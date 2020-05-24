676e07797531fd7bc30bd81c7b6210707fe2a4a7
Nofar Baranes <nofar@reallygood.co.il>
2020-05-24 12:44:35 +0300

    Setting dynamic iframe src by courseId

--- src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
+++ src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
@@ -1,9 +1,14 @@
 import React, { useContext, useEffect } from "react";
+import { RouteComponentProps } from "react-router-dom";
+
 import { TeachMeContext } from "../../../../App";
 
-export default function QuizScreen() {
+type TParams = { courseId: string };
+
+export default function QuizScreen({ match }: RouteComponentProps<TParams>) {
   const { sidebar } = useContext(TeachMeContext);
   const { isOpen, setIsOpen } = sidebar;
+  const { courseId } = match.params;
 
   useEffect(() => {
     if (isOpen) {
@@ -16,7 +21,7 @@ export default function QuizScreen() {
   return (
     <section className="screen quiz-screen">
       <iframe
-        src="http://localhost:9001/?platform=mock&teachme=mock&courseId=1#"
+        src={`http://localhost:9001/?platform=mock&teachme=mock&courseId=${courseId}`}
         frameBorder="0"
       ></iframe>
     </section>
