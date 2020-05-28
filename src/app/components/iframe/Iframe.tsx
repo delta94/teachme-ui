71fdb55b9b3c9babd590c758bedb66517dc83c5c
Nofar Baranes <nofar@reallygood.co.il>
2020-05-28 11:26:24 +0300

    Using postMessage to send data to iframe

--- src/app/components/iframe/Iframe.tsx
+++ src/app/components/iframe/Iframe.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Iframe({
   isResponsive,
   src,
+  data,
 }: {
   src: string;
   isResponsive?: boolean;
+  data?: any;
 }) {
+  const iframe = React.useRef<HTMLIFrameElement>(null);
   const responsiveClass = isResponsive ? "responsive" : "";
+  const iframeLoaded = () => {
+    if (data) {
+      console.log("iframeLoaded data ", data);
+      const iframeWin = iframe.current.contentWindow;
+      iframeWin.postMessage({ data, type: "loadData" }, src);
+    }
+  };
+
   return (
     <section className={`iframe-container ${responsiveClass}`}>
-      <iframe src={src} frameBorder="0"></iframe>
+      <iframe
+        ref={iframe}
+        src={src}
+        frameBorder="0"
+        onLoad={iframeLoaded}
+      ></iframe>
     </section>
   );
 }
--- src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
+++ src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
@@ -1,18 +1,20 @@
-import React, { useContext, useEffect } from "react";
-import { RouteComponentProps } from "react-router-dom";
+import React, { useContext, useEffect, useState } from "react";
+import { RouteComponentProps, useLocation } from "react-router-dom";
 
 import { TeachMeContext } from "../../../../App";
 import Iframe from "../../../../components/iframe/Iframe";
+import { getCourseById } from "../../../../utils/coursesUtils";
 
 type TParams = { courseId: string };
 
 export default function QuizScreen({ match }: RouteComponentProps<TParams>) {
+  const tmContext = useContext(TeachMeContext);
+  const { tmCourses } = tmContext.tmState;
   const { sidebar } = useContext(TeachMeContext);
   const { isOpen, setIsOpen } = sidebar;
   const { courseId } = match.params;
-  const quizBaseUrl = "https://cdn.walkme.com/apps/wm-forms/index.html";
-  const quizPlatform = "mock"; //TODO: Change to dynamic data
-  const teachmePlatform = "mock"; //TODO: Change to dynamic data
+  const quizBaseUrl = "http://localhost:9001/"; // "https://cdn.walkme.com/apps/wm-forms/index.html";
+  const [quiz, setQuiz] = useState(null);
 
   useEffect(() => {
     if (isOpen) {
@@ -20,14 +22,20 @@ export default function QuizScreen({ match }: RouteComponentProps<TParams>) {
         setIsOpen(false);
       }, 0);
     }
+
+    const currentQuiz = getCourseById({
+      tmCourses,
+      id: courseId,
+    }).quiz;
+
+    if (currentQuiz) {
+      setQuiz(currentQuiz);
+    }
   }, []);
 
   return (
     <section className="screen quiz-screen">
-      <Iframe
-        src={`${quizBaseUrl}?platform=${quizPlatform}&teachme=${teachmePlatform}&courseId=${courseId}`}
-        isResponsive
-      />
+      {quiz && <Iframe src={quizBaseUrl} isResponsive data={quiz} />}
     </section>
   );
 }
