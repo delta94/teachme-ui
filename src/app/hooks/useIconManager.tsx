077d62f23bfd45b9ab3512963a080dc957769837
Nofar Baranes <nofar@reallygood.co.il>
2020-06-01 11:51:26 +0300

    Extract variable iconComponent to external file

--- src/app/hooks/useIconManager.tsx
+++ src/app/hooks/useIconManager.tsx
@@ -1,13 +1,6 @@
 import React from "react";
 import { CourseState, TaskIcon } from "../interfaces/courses/courses.interface";
-import { ReactComponent as CheckIcon } from "../../images/icons/checkmark.svg";
-import { ReactComponent as CloseIcon } from "../../images/icons/close.svg";
-import { ReactComponent as ArrowIcon } from "../../images/icons/arrow-down.svg";
-import { ReactComponent as ArticleIcon } from "../../images/icons/Article.svg";
-import { ReactComponent as VideoIcon } from "../../images/icons/Video.svg";
-import { ReactComponent as WalkthruIcon } from "../../images/icons/Walkthru.svg";
-import { ReactComponent as DoubleCheckmarksIcon } from "../../images/icons/double-checkmarks.svg";
-import { ReactComponent as BackArrowIcon } from "../../images/icons/back-arrow.svg";
+import { iconComponent } from "../consts/icons";
 
 export enum Icon {
   Check = "check",
@@ -22,24 +15,6 @@ export enum Icon {
 }
 
 export type IconType = Icon | CourseState | TaskIcon;
-
-const iconComponent = {
-  check: CheckIcon,
-  completed: CheckIcon,
-  success: CheckIcon,
-  error: CloseIcon,
-  close: CloseIcon,
-  article: ArticleIcon,
-  video: VideoIcon,
-  "smart-walkthru": WalkthruIcon,
-  "arrow-left": ArrowIcon,
-  "arrow-down": ArrowIcon,
-  "arrow-right": ArrowIcon,
-  "arrow-top": ArrowIcon,
-  dropdown: ArrowIcon,
-  tested: DoubleCheckmarksIcon,
-  "back-arrow": BackArrowIcon,
-};
 
 export default function useIconManager(): {
   getIconByType: (type: IconType) => JSX.Element;
