69c88c3e94c386245c681c25af57746233b721c1
Nofar Baranes <nofar@reallygood.co.il>
2020-06-11 19:25:56 +0300

    Adding comment

--- src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
+++ src/app/layout/screens/courses-screen/quiz-screen/QuizScreen.tsx
@@ -16,6 +16,10 @@ export default function QuizScreen({ match }: RouteComponentProps<TParams>) {
   const { sidebar } = useContext(TeachMeContext);
   const { isOpen, setIsOpen } = sidebar;
   const { courseId } = match.params;
+  /**
+   * while develop run teachme project and use this url
+   * localhost url: "http://localhost:9001/";
+   */
   const quizBaseUrl = "https://cdn.walkme.com/apps/wm-forms/index.html";
   const urlParams = window.location.search;
   const quizSrc = `${quizBaseUrl}${urlParams}&courseId=${courseId}`;
